b3cb5b011524a27c9b4064af1c86145642a3dbc8
Thomas Csigai <thomascsigai1@gmail.com>
2024-06-03 20:20:46 +0200

    Adding input + window color

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
 
 int main()
 {
@@ -33,6 +34,14 @@ int main()
 
     while (!glfwWindowShouldClose(window))
     {
+        // input
+        processInput(window);
+
+        // rendering commands
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // check and call events and swap the buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -44,4 +53,10 @@ int main()
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
