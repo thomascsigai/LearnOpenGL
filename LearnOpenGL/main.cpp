2496b60202b185332d5e8cc7f0be585618cde405
Thomas Csigai <thomascsigai1@gmail.com>
2024-06-07 15:42:11 +0200

    Printing two triangles next to each others

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -49,24 +49,15 @@ int main()
     // vertex data and buffers
     // ----------------------------------------
 
-    // vertex data to render a rectangle
+    // vertex data to render a triangle
     float vertices[] = {
-      0.5f,  0.5f, 0.0f,  // top right
-      0.5f, -0.5f, 0.0f,  // bottom right
-     -0.5f, -0.5f, 0.0f,  // bottom left
-     -0.5f,  0.5f, 0.0f   // top left 
+    -0.8f, -0.5f, 0.0f,
+    -0.5f, 0.5f, 0.0f,
+    -0.2f,  -0.5f, 0.0f,
+    0.8f, -0.5f, 0.0f,
+    0.5f, 0.5f, 0.0f,
+    0.2f,  -0.5f, 0.0f
     };
-
-    unsigned int indices[] = {  // note that we start from 0!
-    0, 1, 3,   // first triangle
-    1, 2, 3    // second triangle
-    };
-
-    unsigned int EBO;
-    glGenBuffers(1, &EBO);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
@@ -150,9 +141,6 @@ int main()
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    // uncomment this call to draw in wireframe polygons.
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
     // render loop
     // ----------------------------------------
     while (!glfwWindowShouldClose(window))
@@ -165,8 +153,8 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
 
         // check and call events and swap the buffers
         glfwSwapBuffers(window);
